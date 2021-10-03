e3cfb50e11f07da276dfb2ecc5ff987067daafeb
Saulo Contente <contente.saulo@gmail.com>
2021-10-03 14:10:11 -0300

    Implements a connection project with Firebase

---
+++ app/src/main/java/com/aulaudemy/whatsappcloneandroid/MainActivity.java
@@ -0,0 +1,20 @@
+package com.aulaudemy.whatsappcloneandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        reference.child("pontuação").setValue(100);
+    }
+}
